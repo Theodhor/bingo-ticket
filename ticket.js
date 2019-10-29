d81fc925ed86144a777822eca29910d7a8fb14fa
Theodhor <s.theodhor@yahoo.it>
2019-10-29 20:32:40 +0000

    minor changes and commented .js file

--- ticket.js
+++ ticket.js
@@ -1,59 +1,74 @@
-let initial = 1;
-let counter = 0;
-
-function makeColumns(){
-  const columns = [];
-  for(let a=0; a<9; a++){
-    const column ={};
-    column.column = a;
-    columns.push(column);
-    const pool =[];
-    let range;
-    if(a===0){
-      range = 9;
-    } else if(a===8){
-      range = 11;
-    } else range = 10;
-    const final = initial + range;
-    for(let b=initial; b<final; b++){
-      pool.push(b);
-    }
-    initial = final;
-    column.pool = pool;
-    column.counter = 3;
-    column.range = range;
-    column.numbers =[];
-    column.positions =[];
-  }
-  return columns;
-}
-
-
-function makeNumber(column){
-  const index = Math.floor(Math.random() * column.range);
-  const number = column.pool[index];
-  column.counter--;
-  column.range--;
-  column.pool.splice(index,1);
-  return number;
-}
-
-function assignPoistion(){
-  let position;
-  if(counter<5){
-    position = 0;
-  } else if(counter<10){
-    position = 1;
-  } else position = 2;
-  counter++;
-  return position;
-}
-function createIndexArray(){
-  return [0,1,2,3,4,5,6,7,8];
-}
+let ticketSelector;
+/*  this function returns an object that can be used to easily display data in
+the DOM and to check conditions for a bingo game  */
 function makeTicket(){
+  let initial = 1; // is the first number between 1 and 90
+  let counter = 0;
+
+  /* returns an array of length 9, each element of the array contains:
+  pool => array of numbers that can be randomly selected
+  range => length of the array pool
+  counter => indicates how may numbers can be selected
+  numbers => array that includes the numbers selected
+  positions => array of keys related to array numbers, useful to create ticket rows
+   */
+  function makeColumns(){
+    const columns = [];
+    for(let a=0; a<9; a++){
+      const column ={};
+      column.column = a;
+      columns.push(column);
+      const pool =[];
+      let range;
+      if(a===0){
+        range = 9;
+      } else if(a===8){
+        range = 11;
+      } else range = 10;
+      const final = initial + range;
+      for(let b=initial; b<final; b++){
+        pool.push(b);
+      }
+      initial = final;
+      column.pool = pool;
+      column.counter = 3;
+      column.range = range;
+      column.numbers =[];
+      column.positions =[];
+    }
+    return columns;
+  }
+  /* removes a number from column.pool and returns it */
+  function makeNumber(column){
+    const index = Math.floor(Math.random() * column.range);
+    const number = column.pool[index];
+    column.counter--;
+    column.range--;
+    column.pool.splice(index,1);
+    return number;
+  }
+  /* the property position will be useful to create the ticket rows */
+  function assignPoistion(){
+    let position;
+    if(counter<5){
+      position = 0;
+    } else if(counter<10){
+      position = 1;
+    } else position = 2;
+    counter++;
+    return position;
+  }
+  /* returns an array of length 9 */
+  function createIndexArray(){
+    return [0,1,2,3,4,5,6,7,8];
+  }
+
+
   const columns = makeColumns();
   let indexArray = createIndexArray();
+  /* this loop selects a random number from each column, 9 in total
+  allowing to have in every ticket at least 1 number from each
+  column */
   for(let a=0; a<9; a++){
     const index = Math.floor(Math.random() * indexArray.length);
     const number = makeNumber(columns[indexArray[index]]);
@@ -65,17 +80,20 @@ function makeTicket(){
   indexArray = createIndexArray();
 
   let index;
+  /* this loop selects the remaining 6 numbers */
   for(let a=0;a<6;a++){
-    let switcher = true;
-    while(switcher){
+    /* switches if the column selected has less than 3 numbers already assigned to the tickets
+    and also the positionsArray of the column does not already include the new position (*1) */
+    let validatorBoolean = true;
+    while(validatorBoolean){
       index = Math.floor(Math.random() * indexArray.length);
       if(columns[indexArray[index]].counter>0){
         const number =makeNumber(columns[indexArray[index]]);
-        const position = assignPoistion();
+        const position = assignPoistion(); // (*1)
         if(!columns[indexArray[index]].positions.includes(position)){
           columns[indexArray[index]].numbers.push(number);
           columns[indexArray[index]].positions.push(position);
-          switcher=!switcher;
+          validatorBoolean=!validatorBoolean;
         } else counter--;
 
       }
@@ -97,19 +115,9 @@ function makeTicket(){
       ticket.rows[column.positions[a]].push(number);
     }
   });
-  initial = 1;
-  counter = 0;
   return ticket;
 }
-const array =[];
-for(let a=1; a<100; a++){
-  const ticket = makeTicket();
-  array.push(ticket);
-}
-console.log(array[0]);
-document.addEventListener('DOMContentLoaded', function(){
-  const domTicket = document.querySelector('.ticket');
-  const analyzed = array[0];
+function makeDomTicket(entry){
   const body = document.createElement('div');
   body.classList.add('ticket-body');
   for(let a=0;a<9;a++){
@@ -119,7 +127,7 @@ document.addEventListener('DOMContentLoaded', function(){
       let matched = false;
       const square = document.createElement('div');
       square.classList.add('square');
-      analyzed.numbers.forEach(function(number){
+      entry.numbers.forEach(function(number){
         if(number.column===a && number.position===b){
           square.classList.add('full');
           square.innerText=number.number;
@@ -133,5 +141,20 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     body.appendChild(column);
   }
-  domTicket.appendChild(body);
+  return body;
+}
+document.addEventListener('DOMContentLoaded', function(){
+  const domTicket = document.querySelector('.ticket');
+  let analyzed = makeTicket();
+  let newTicket = makeDomTicket(analyzed);
+  const button = document.querySelector('.button');
+  domTicket.appendChild(newTicket);
+  ticketSelector = document.querySelector('.ticket-body');
+  button.addEventListener('click', function(){
+    ticketSelector.remove();
+    analyzed = makeTicket();
+    newTicket = makeDomTicket(analyzed);
+    domTicket.appendChild(newTicket);
+    ticketSelector = document.querySelector('.ticket-body');
+  });
 });
